7f22f267efeb40158ad1144f011cf1876ba521f3
Jean Paulo dos Santos <joaocarlos.santos14@gmail.com>
2024-04-14 13:05:21 -0300

    Feat: Adicionado loop para execuçao do que vai ser pesquisado. Adicionado classe do ChromeDriver.

--- Aplicacao/Program.cs
+++ Aplicacao/Program.cs
@@ -1,8 +1,10 @@
-using RPA.AeC.Alura;
+using RPA.AeC.Alura.Aplicacao;
+using RPA.AeC.Alura.Dominio.Driver;
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
+        services.AddScoped<IChromeDriver, ChromeDriver>();
         services.AddHostedService<Worker>();
     })
     .Build();
---
+++ Aplicacao/Worker.cs
@@ -0,0 +1,77 @@
+using Microsoft.Extensions.DependencyInjection;
+using RPA.AeC.Alura.Dominio.Driver;
+
+namespace RPA.AeC.Alura.Aplicacao
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+        private int _task;
+        private IServiceProvider _serviceProvider;
+
+        public Worker(ILogger<Worker> logger, IServiceProvider serviceProvider, IConfiguration configuration)
+        {
+            _serviceProvider = serviceProvider;
+            _logger = logger;
+            _task = Convert.ToInt32(configuration["NumberOfTask"]);
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            List<string> listaCurso = new List<string>
+            {
+                "RPA"
+            };
+
+            try
+            {
+                while (!stoppingToken.IsCancellationRequested && listaCurso.Count > 0)
+                {
+                    if (listaCurso.Count > 0)
+                    {
+                        Task[] tasks = new Task[_task];
+
+                        for (int i = 0; i < tasks.Length && listaCurso.Count > 0; i++)
+                        {
+                            string categoria = listaCurso[0];
+                            listaCurso.RemoveAt(0);
+
+                            tasks[i] = Task.Factory.StartNew(() => DoWork(categoria, stoppingToken));
+                        }
+                        await Task.WhenAll(tasks);
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Erro durante a execução.");
+            }
+
+            _logger.LogInformation("Serviço finalizado: {time}", DateTimeOffset.Now);
+            await Task.Delay(1000, stoppingToken);
+        }
+
+        private void DoWork(string categoria, CancellationToken stoppingToken) 
+        {
+            using IServiceScope scope = _serviceProvider.CreateScope();
+
+            IChromeDriver _chrome = scope.ServiceProvider.GetRequiredService<IChromeDriver>();
+
+            try
+            {
+                var driver = _chrome.IniciarChromeDriver();
+                if (driver == null);
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"ERRO -> {ex.Message}");
+            }
+            finally
+            {
+                _chrome.FinalizarChromeDriver();
+            }
+        }
+    }
+}
